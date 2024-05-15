96e3d978c1e13d87bfe672d0332e5f52196ffb10
Victor Buendia <victorbuendia03@gmail.com>
2024-05-15 13:50:13 -0300

    refactor: Update Terraform backend key for Consultant API deployment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket          = "cryptobotunb-terraform-prod-state"
-    key             = "terraform/wallet_api_terraform.tfstate"
+    key             = "terraform/consultant_api_terraform.tfstate"
     region          = "us-east-1"
     dynamodb_table  = "cryptobotunb-terraform-prod-db"
     encrypt         = true
