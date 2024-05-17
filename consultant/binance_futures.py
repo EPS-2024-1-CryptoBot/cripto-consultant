c1a893038d3d2b67ac187a176b1437c7dc2888f0
jvsdurso <jvsdurso@gmail.com>
2024-05-17 03:54:47 -0300

    Pegando as moedas existentes na Binance

--- consultant/binance_futures.py
+++ consultant/binance_futures.py
@@ -1,6 +1,20 @@
 import logging
+import pprint
+
+import requests
 
 logger = logging.getLogger()
 
-def write_log():
-  logger.info("HellO!")
+
+def get_contracts():
+
+    response_object = requests.get("https://fapi.binance.com/fapi/v1/exchangeInfo")
+
+    contracts = []
+
+    for contract in response_object.json()['symbols']:
+        contracts.append(contract['pair'])
+
+    return contracts
+
+print(get_contracts())
