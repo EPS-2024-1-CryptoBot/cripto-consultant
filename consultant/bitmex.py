b16f02232677d4899011758573458926e026db3f
jvsdurso <jvsdurso@gmail.com>
2024-05-17 04:12:40 -0300

    Pegando as moedas existentes pela Bitmex

---
+++ consultant/bitmex.py
@@ -0,0 +1,20 @@
+import logging
+import pprint
+
+import requests
+
+logger = logging.getLogger()
+
+
+def get_contracts():
+
+    response_object = requests.get("https://www.bitmex.com/api/v1/instrument/active")
+
+    contracts = []
+
+    for contract in response_object.json():
+        contracts.append(contract['symbol'])
+
+    return contracts
+
+print(get_contracts())
