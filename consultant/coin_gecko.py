865ae168e8fc57c05cf92bb5b16fecb8993f48c4
Kevin Luis <k.luis360@gmail.com>
2024-05-16 23:19:12 -0300

    feat: Add CoinGecko API integration for cryptocurrency data

---
+++ consultant/coin_gecko.py
@@ -0,0 +1,56 @@
+import os
+import requests
+
+"""
+REFER TO THE COIN GECKO API DOCUMENTATION FOR MORE INFORMATION (V3.0.1)
+https://docs.coingecko.com/v3.0.1/reference/introduction
+"""
+
+def build_gecko_request(method, endpoint, params=None):
+    url = os.environ.get("COIN_GECKO_API_URL") + endpoint
+
+    headers = {
+        "accept": "application/json",
+        "x-cg-demo-api-key": os.environ.get("COIN_GECKO_API_KEY")
+    }
+
+    if method == "get":
+        response = requests.get(url, headers=headers, params=params)
+    elif method == "post":
+        response = requests.post(url, headers=headers, json=params)
+    else:
+        response = None
+
+    return response
+
+
+
+def get_coin_gecko_info():
+    return {"info": "CoinGecko info", 
+                "key": os.environ.get("COIN_GECKO_API_KEY"),
+                "url": os.environ.get("COIN_GECKO_API_URL")
+            }
+
+def get_coin_gecko_health():
+    response = build_gecko_request("get", "/ping")
+    return response.json()
+
+def get_coins_list():
+    response = build_gecko_request("get", "/coins/list")
+    return response.json()
+    
+def get_coin_simple_price(coin_id):
+    """
+    Get the current price of a coin in USD
+    This endpoint accept array of ids in a single request, for the MVP we are going to use only one id
+    """
+    response = build_gecko_request("get", f"/simple/price?ids={coin_id}&vs_currencies=brl&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true&precision=0")
+    return response.json()
+
+def get_coin_chart_data_with_timestamp(coin_id):
+    """
+    Get the market chart data (price, market cap, volume) of a coin in a specific date range
+    For now leaving the default range of 3 weeks
+    """
+    response = build_gecko_request("get", f"/coins/{coin_id}/market_chart/range?vs_currency=usd&from=1711929600&to=1712275200")
+    return response.json()
--- consultant/main.py
+++ consultant/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from mangum import Mangum
+import coin_gecko
 
 app = FastAPI()
+
 handler = Mangum(app)
 
 @app.get("/")
@@ -10,6 +12,36 @@ def read_root():
     This is the root path of the API
     """
     return {"ConsultantAPI": "CryptoBot_UnB_2024.1"}
+
+@app.get("/crypto_currency/info")
+def get_crypto_currency_api_info():
+    """
+    Path to get the CoinGecko API information
+    """
+    return coin_gecko.get_coin_gecko_health()
+
+@app.get("/crypto_currency/coins/list")
+def get_crypto_currency_coins_list():
+    """
+    Path to get the list of coins ids from CoinGecko
+    CoinGecko spreadsheet in case you want to check the list of 
+    coins without calling the API: https://docs.google.com/spreadsheets/d/1wTTuxXt8n9q7C4NDXqQpI3wpKu1_5bGVmP9Xz0XGSyU/edit#gid=0
+    """
+    return coin_gecko.get_coins_list()
+
+@app.get("/crypto_currency/coins/{coin_id}")
+def get_crypto_currency_coin(coin_id: str):
+    """
+    Path to get the information of a specific coin from CoinGecko
+    """
+    return coin_gecko.get_coin_simple_price(coin_id)
+
+@app.get("/crypto_currency/coins/{coin_id}/chart")
+def get_crypto_currency_coin_chart(coin_id: str):
+    """
+    Path to get the chart data of a specific coin from CoinGecko
+    """
+    return coin_gecko.get_coin_chart_data_with_timestamp(coin_id)
 
 if __name__ == "__main__":
     import uvicorn
