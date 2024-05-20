0b21e3bcccd22c614882042ba56d38ca747da196
Victor Buendia <victorbuendia03@gmail.com>
2024-05-20 19:48:55 -0300

    feat: Add environment variables for CoinGecko API integration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,13 @@ variable "environment" {
     type = string
     sensitive = false
 }
+
+variable "COIN_GECKO_API_KEY" {
+    type = string
+    sensitive = true
+}
+
+variable "COIN_GECKO_API_URL" {
+    type = string
+    sensitive = false
+}
